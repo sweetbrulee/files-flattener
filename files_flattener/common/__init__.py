0b276eca4cb14215af318703173d6050298dcdb3
sweetbrulee <sparrowking002@gmail.com>
2024-07-24 20:47:53 +0800

    chore: Fix usage text

--- files_flattener/common/__init__.py
+++ files_flattener/common/__init__.py
@@ -3,13 +3,13 @@ from .logger import logger_instance as logger
 __all__ = ["logger"]
 
 USAGE = """
-Usage: flt <directory> <output_file> [<ignore_file>]
+Usage: flt <identifier> <output_file> [<ignore_file>]
 
 Parameters:
-  <directory>    : The path of the directory containing the files to be flattened.
+  <identifier>   : The path of the local directory or the URL of the remote repository containing the files to be flattened.
   <output_file>  : The path of the output file where the contents of the files will be written.
   [<ignore_file>]: (Optional) The path to a file containing patterns of files to ignore.
-                   If not provided, the script will look for a '.ignore' file in the specified directory.
+                   If not provided, the script will look for a '.ignore' file under the specified identifier path.
                    If the '.ignore' file is not found, no files will be ignored.
 """
 
